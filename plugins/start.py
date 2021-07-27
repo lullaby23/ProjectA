8413c1a1bfdb835469bd0a42c6de3a705157e63f
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2021-07-28 01:28:11 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -105,7 +105,7 @@ async def not_joined(client: Client, message: Message):
      [
                 [
                     InlineKeyboardButton("Join Channel 🔑", url = client.invitelink),
-                    InlineKeyboardButton("😊 Cara Pakai Bot", url = callback_data.about)
+                    InlineKeyboardButton("😊 Cara Pakai Bot", callback_data = "about")
                  ]
             ]
         )
