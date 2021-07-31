05f7202b604d9e0a52feda39a68f9c8e85ea076b
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2021-07-31 10:59:31 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -95,7 +95,7 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>Hallo Sayang 👋</b> Kamu Harus Join Channel Atau Group Dulu Ya Sayang ( づ￣ ³￣ )づ \nKalau Sudah Join Tekan Tulisan 🔒 Buka File 🔒 Kemudian tekan tombol Start\n"
+    text = "<b>Hallo Sayang 👋</b> Kamu Harus Join Channel Atau Group Dulu Ya Sayang. \nKalau Sudah Join Tekan Tulisan 🔒 Buka File 🔒 Kemudian tekan tombol Start\n"
     message_text = message.text
     try:
         command, argument = message_text.split()
